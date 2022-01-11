653a30d233d7e483453eb419a0a32f479e04ab49
EvgeniPichurin <pichurinaa@gmail.com>
2022-01-11 19:15:00 +0200

    HW_10

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
-console.log("Hello World");
-// //Output for var: 3 3 3
-// for (var i = 0; i < 3; i++){
-//     setTimeout(function(){
-//         console.log(i);
-//     })
-// }
-// //Output for let: 0 1 2
-// for (let i = 0; i < 3; i++){
-//     setTimeout(function(){
-//         console.log(i);
-//     })
-// }
-const fun = function(a,b) {
-    return a + b;
+// Task 1
+console.log("Task 1:");
+
+console.log("a" + ("a" - "s") + "a" + "s");
+
+// Task 2
+console.log("Task 2:");
+
+function calculate(num1, num2, option){
+    let result = "Wrong option!";
+    switch(option){
+        case 1: result = num1 * num2;
+        break;
+        case 2: 
+        if (num2 !== 0) result = num1 / num2; 
+        break;
+        case 3: result = num1 + num2; 
+        break;
+        case 4: result = num1 - num2; 
+        break;
+    }
+    return result;
 }
-let a = 5;
-// console.log(a ** 2);
-// console.log(fun(10,20));
-// console.log(a(10,20)); // error
-// console.log(fun ** 2); // NaN - Not a Number
-console.log("12" + 12); // JS defines that operator + exists for strings, that's why number 12 will be converted to string
-console.log("12" - 12); // JS defines that operator - doesn't exist for string, that's why string "12" will be converted to number
-console.log("ab" - 12); // ...NaN
-//sometimes there is a need to explicit converion from string to a number
-//"+" unary is the simplest way for the explicit conversion of a string to a number
-console.log("12" + 12);
-console.log(`+"12" + 12 = ${+"12" + 12}`);
-// HW #definition
-// 1 - using only two letters "a", "s" print out word ananas case insensitive
-// 2 - write function calculate that can perform any arithmetic operation on two numbers (*, /, +, -)
-// 3 - write any example for running the following expression fun(5)(10, 3) 
+let A = calculate(4,1,10); //Example
+console.log(A);
+
+// Task 3
+console.log("Task 3:");
+
+function fun(x) {
+    function inFun(y, z) {
+      return x * (y ** z);
+    }
+    return inFun;
+}
+
+let mainFunction = fun(5);
+const result = fun(5)(10, 3); 
+
+console.log(result);
