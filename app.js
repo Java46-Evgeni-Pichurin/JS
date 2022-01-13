a4660489e299291a7a50273d0c1f984afed1c9d6
EvgeniPichurin <pichurinaa@gmail.com>
2022-01-13 21:13:04 +0200

    classwork

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-// Task 1
-console.log("Task 1:");
-
-console.log("a" + ("a" - "s") + "a" + "s");
-
-// Task 2
-console.log("Task 2:");
-
-function calculate(num1, num2, option){
-    let result = "Wrong option!";
-    switch(option){
-        case 1: result = num1 * num2;
-        break;
-        case 2: 
-        if (num2 !== 0) result = num1 / num2; 
-        break;
-        case 3: result = num1 + num2; 
-        break;
-        case 4: result = num1 - num2; 
-        break;
-    }
-    return result;
+/*Strings*/
+const str1 ="abc";
+const str2 = "abc";
+console.log(`str1 == str2 is ${str1 == str2}`);
+console.log(`"abc" < "ab" is ${"abc" < "ab"}`);
+console.log(`"123" > 23 is ${"123" > 23}`);
+console.log(`"123" > 23 is ${"123" > "23"}`);
+// If logical expression cntains both string and number, JS will convert string to number
+// If string doesn't contain a number a result of conversion will be NaN
+// In any logical expression if there is NaN, then the result will be false
+console.log(`"abc" > 23 is ${"abc" > 23}`);// false
+console.log(`"abc" < 23 is ${"abc" < 23}`);// false
+console.log(`"abc" != 23 is ${"abc" != 23}`);// true
+/*********************************/
+function stringProcessin(str) {
+    const strP = "" + str;
+    console.log(`"${str}"[4] is ${str[4]}`);
+    console.log(`length of "${strP}" is ${strP.length}`);
+    console.log(`"${strP}" includes "abc" is ${strP.includes("abc")}`);
+    console.log(`substring of "${strP}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
+    console.log(`index of substring "ll" in the "${strP}" is ${strP.indexOf("ll")}`);
+    console.log(`last index of substring "ll" in the "${strP}" is ${strP.lastIndexOf("ll")}`);
 }
-let A = calculate(4,1,10); //Example
-console.log(A);
+stringProcessin("abcd*lmn*ll(oo&&ll");
 
-// Task 3
-console.log("Task 3:");
 
-function fun(x) {
-    function inFun(y, z) {
-      return x * (y ** z);
-    }
-    return inFun;
+/*********************************************************
+write function:
+
+function encode (num, codingString) {
+    // codingString - any string with no repeated symbols
+    // you should validate codingString doesn't contain repeated symbols
+    // base = length of codingString
+    // algorithm the same as specified above
+    // getSymbol(digit, codingString) using operator [i]
+}
+ ********************************************************/
+
+function encode(num, base) {
+    // base from 2 to 10
+    let res="";
+    do {
+        const digit = Math.trunc(num % base);
+        const symb = getSymbol(digit);
+        res = symb + res;
+        num = Math.trunc(num / base);
+    } while(num >= 1);
+    return res;
 }
 
-let mainFunction = fun(5);
-const result = fun(5)(10, 3); 
+function getSymbol(digit) {
+    // if base from 2 to 10
+    console.log(digit);
+    return "" + digit; // it will work only with base <= 10
+}
 
-console.log(result);
+console.log(encode(10, 2));
