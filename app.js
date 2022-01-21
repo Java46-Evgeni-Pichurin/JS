778c55fc32f79f02f6bdfe3f10df0b02f76954db
EvgeniPichurin <pichurinaa@gmail.com>
2022-01-21 08:47:44 +0200

    classwork

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
-console.log(`\nTask 1:`);
-const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
-const left = arHw.filter((a) => a % 2 == 0).sort((a,b) => a - b);
-const right = arHw.filter((a) => a % 2 != 0).sort((a,b) => b - a);
-arHw.splice(0, arHw.length, left.concat(right));
-console.log(`Sorted array arHw is [${arHw}]\n`);
-
-console.log(`Task 2:`);
-function displayMatrix(matrix) {
-     for (let i = 0; i < matrix.length; i++) {
-        let row = '';
-        for (let j = 0; j < matrix[i].length; j++) {
-            row = row + matrix[i][j] + '   '
-        }
-        console.log(row);
-     }  
+function mapper (n, i, a) {
+    return `<li>${i + 1} of ${a.length} - ${n}</li>`    
 }
-function matrixTransp(matrix){
-    let transposeMatrix = [];
-    let rows, columns;
-    for(let i = 0; i < matrix.length; i++){
-        rows = matrix.length;
-        for (let j = 0; j < matrix[i].length; j++) {
-            columns = matrix[i].length;
-        }
-    };
-    for (let i = 0; i < Math.max(rows,columns); i++) {
-        transposeMatrix.push([]);
-    }
-    for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[i].length; j++){
-            transposeMatrix[j].push(matrix[i][j]);
-        };
-    };
-    return transposeMatrix;
-}
+function someFunction() {
+     const ar = [-10, 50, -12, 80, 90];
+     const ar3 = ar.map(mapper);
+console.log(ar3);
 
-const matrix1 = [[11, 10, 20, 30], [12, 50, -5, 20], [13, 14, 34, 28]];
-console.log(`${displayMatrix(matrix1)} \nTransposed matrix:\n`);
-console.log(`${displayMatrix(matrixTransp(matrix1))}`)
+}
+const ar = [-10, 50, -12, 80, 40];
+ar.push(70);
+// str - "-10#50#-12#80#40#70"
+/*********solution based on substring */
+// let str = '';
+// ar.forEach(function(n) {
+//         str += n + '#';
+// })
+// str = str.substring(0, str.length - 1);
+/*************************solution based on forEach form second number */
+// const ar1 = ar.slice(1);
+// let str =  '' + ar[0];
+// ar1.forEach(n => str += '#' + n);
+// console.log(str);
+/*****************************printing out sequense number of element, element, length of arry */
+// 1 of 5 - -10;    ....
+ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
+
+// method "map"
+//use case of applying method map : you want to create new array with elements that are received as result of some conversion
+//example you want to get new array with elements that are muliplication on 2 of each source element
+const ar2 = ar.map(n => n * 2);
+console.log(ar2);
+someFunction();
+/*****************HW 13 task1 definition */
+// write function myForEach(array, callback-function);
+//array  - being iterated array
+//callback-function - function that will be called for each element of array
+//callback-function should take three arguments: current elemnt, current index, being iterated array
+//example of standard forEach : array.forEach(n => str += '#' + n);
+//example of myForEach : myForEach(array, n => str += '#' + n);
+
+/********************************************************************* */
+/**********************************************HW 13 definition task2 */
+//write method myMap  for the same functionality as standard method map
+//function myMap(array, callback-function)
+//myMap will apply your method myForEach
