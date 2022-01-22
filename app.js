5204e3577558accde6d6c6bcd195708424b1724f
EvgeniPichurin <pichurinaa@gmail.com>
2022-01-22 21:01:20 +0200

    HW_13

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-function mapper (n, i, a) {
-    return `<li>${i + 1} of ${a.length} - ${n}</li>`    
-}
-function someFunction() {
-     const ar = [-10, 50, -12, 80, 90];
-     const ar3 = ar.map(mapper);
-console.log(ar3);
+console.log(`\nTask 1:\n`);
 
-}
-const ar = [-10, 50, -12, 80, 40];
-ar.push(70);
-// str - "-10#50#-12#80#40#70"
-/*********solution based on substring */
-// let str = '';
-// ar.forEach(function(n) {
-//         str += n + '#';
-// })
-// str = str.substring(0, str.length - 1);
-/*************************solution based on forEach form second number */
-// const ar1 = ar.slice(1);
-// let str =  '' + ar[0];
-// ar1.forEach(n => str += '#' + n);
-// console.log(str);
-/*****************************printing out sequense number of element, element, length of arry */
-// 1 of 5 - -10;    ....
-ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
+let myForEach = function(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+      callback(array[i], i, array);
+    }
+};
 
-// method "map"
-//use case of applying method map : you want to create new array with elements that are received as result of some conversion
-//example you want to get new array with elements that are muliplication on 2 of each source element
-const ar2 = ar.map(n => n * 2);
-console.log(ar2);
-someFunction();
-/*****************HW 13 task1 definition */
-// write function myForEach(array, callback-function);
-//array  - being iterated array
-//callback-function - function that will be called for each element of array
-//callback-function should take three arguments: current elemnt, current index, being iterated array
-//example of standard forEach : array.forEach(n => str += '#' + n);
-//example of myForEach : myForEach(array, n => str += '#' + n);
+const arrEx1 = [1, 2, 3, 4, 5, 6, 7];
 
-/********************************************************************* */
-/**********************************************HW 13 definition task2 */
-//write method myMap  for the same functionality as standard method map
-//function myMap(array, callback-function)
-//myMap will apply your method myForEach
+myForEach(arrEx1, (n, i, a) => {
+    let newVal = Math.pow(n, i);
+    console.log('Value: ' + n + ', index: ' + i + `, value in the power of its index: ` + newVal); //Example
+});
+
+console.log(`\nTask 2:\n`);
+
+let myMap = function(array, callback) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+      newArr.push(callback(array[i], i, array));
+    }
+    return newArr;
+};
+
+const arrEx2 = [7, 6, 5, 4, 3, 2, 1];
+
+const checkArray = myMap(arrEx2, (n, i, a) => n * 10);
+
+console.log(`Original array: ` + `[` + arrEx2 + `]`);
+console.log(`New array: ` + `[` + checkArray + `]`);
