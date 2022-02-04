6c32949ff08d4bea2c3f6a983e51ede7d23665df
EvgeniPichurin <pichurinaa@gmail.com>
2022-02-04 14:11:07 +0200

    classwork

--- app.js
+++ app.js
@@ -1,74 +1,91 @@
-/*********************************** Task 1 ***************************************/
-console.log(`\nTask 1:\n`);
-
-function Deferred(){
-  this.res;
-  this.arr = [];
-}
-Deferred.prototype.then = function(el) {
-  this.arr.push(el);
-}
-Deferred.prototype.resolve = function(value) {
-  this.res = value;
-  for (let i = 0; i < this.arr.length; i++) {
-      this.res = this.arr[i](this.res);
+class Person {
+  #id;
+  #name;
+  constructor(id, name) {
+          this.#id = id;
+          this.#name = name
+  }
+  getId() {
+          return this.#id;
+  }
+  getName() {
+          return this.#name
+  }
+  toString() {
+          return `id: ${this.#id}; name: ${this.#name};`
   }
 }
-
-const d = new Deferred();
-d.then(function(res){ console.log('1 ', res); return 'a'; });
-d.then(function(res){ console.log('2 ', res); return 'b'; });
-d.then(function(res){ console.log('3 ', res); return 'c'; });
-d.resolve('hello');
-
-/*********************************** Task 2 ***************************************/
-console.log(`\nTask 2:\n`)
-
-function MyArray(n){
-  this.arr = [];
-  this.defaultValue = n;
-}
-// Fill empty elements of array with default value
-MyArray.prototype.fillEmpty = function(){ 
-  for (let i = 0; i < this.arr.length; i++) {
-    this.arr[i] === undefined ? this.arr[i] = this.defaultValue : this.arr[i]
+const person = new Person(123, 'Moshe');
+console.log(`person is ${person}`)
+class Employee extends Person {
+  #salary;
+  constructor(id, name, salary) {
+          super(id, name); //call the constructor of the class Person
+          this.#salary = salary;
+  }
+  computeSalary() {
+          return this.#salary
+  }
+  toString() {
+          return super.toString() + ` salary: ${this.computeSalary()}`
   }
 }
-// Check if parameter "inedex" is bigger than length of array. If yes, sets new length of the array.
-MyArray.prototype.checkIndex = function(index){ 
-  index > this.arr.length ? this.arr.length += index - this.arr.length + 1 : this.arr.length;
-}
-MyArray.prototype.get = function(index) {
-  this.checkIndex(index);
-  this.fillEmpty();
-  return this.arr[index];
-}
-MyArray.prototype.set = function(index, value) {
-  this.checkIndex(index);
-  this.arr[index] = value;
-  this.fillEmpty();
-}
-MyArray.prototype.setValue = function(value) {
-  for (let i = 0; i < this.arr.length; i++) {
-      this.arr[i] = value;
+const person2 = new Employee(124, "Sara", 5000);
+console.log(`person2 is ${person2}`)
+console.log(typeof(person2)) // just object
+console.log(person2.constructor.name) // only this way JS allow getting constructor name
+class Child extends Person {
+  #kindergarten
+  constructor(id, name, kindergarten) {
+       super(id, name);
+       this.#kindergarten = kindergarten   
+  }
+  getKindergarten() {
+          return this.#kindergarten;
+  }
+  toString() {
+          return `${super.toString()} kindergarten: ${this.#kindergarten}`
   }
 }
-MyArray.prototype.toString = function() {
-  return JSON.stringify(this.arr);
+const person3 = new Child(125, 'Yakob', 'Shalom');
+console.log(`person3 is ${person3}`)
+
+class WageEmployee extends Employee {
+  #hours
+  #wage
+  constructor(id, name, salary, hours, wage) {
+          super(id, name, salary)
+          this.#hours = hours;
+          this.#wage = wage;
+  }
+  computeSalary() {
+          return super.computeSalary() + this.#hours * this.#wage
+  }
+  
+
 }
-
-const myArray = new MyArray(7);
-
-console.log(`Array[10] = ${myArray.get(10)}`) // displayed out 7
-myArray.set(5, 66)//sets 66 at index 5
-console.log(myArray.toString());
-
-console.log(`Array[5] = ${myArray.get(5)}`) //displayed out 66
-myArray.setValue(300);
-console.log(myArray.toString());
-
-console.log(`Array[8] = ${myArray.get(8)}`) //displayed out 300
-myArray.setValue(1);
-console.log(`Array[0] = ${myArray.get(0)}`) //displayed out 1
-myArray.set(15, 99)
-console.log(myArray.toString());
+const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
+console.log(`person4 is ${person4}`)
+/**************************************************************HW #17 definition */
+const persons = [
+  new Child(100, 'Olya', 'Shalom'),
+  new Child(101, 'Serega', "Boker"),
+  new Child(102, 'Kolya', 'Shalom'),
+  new Employee(103, 'Vasya', 1000),
+  new WageEmployee(104, 'Tolya', 1000, 10, 100)
+]
+function countOfPersonType(persons, type) {
+  //return count of persons of the given type
+  //Example:
+  //countOfPersonType(persons, 'WageEmployee') ---> 1
+}
+function computeSalaryBudget(persons) {
+  //returns total salary of all employee objects in the given array
+  //Example:
+  //computeSalaryBudget(persons) ---> 3000
+}
+function countChildrenGindergarten(persons, kindergarten) {
+  //returns number of children in the given kindergarten
+  //Example:
+  //countChildrenGindergarten(persons, 'Shalom') ---> 2
+}
